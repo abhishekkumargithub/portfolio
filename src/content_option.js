eb3de12835d25136933fcdefff8c7160acd37a25
Abhishek kumar <91794397+abhishekkumargithub@users.noreply.github.com>
2023-10-18 23:41:59 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,108 @@
+const logotext = "Abhishek Kumar";
+const meta = {
+    title: "Abhishek Kumar",
+    description: "I’m Abhishek Kumar Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Abhishek Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am a Full Stack Developer",
+    },
+    description: "My name is Abhishek Kumar and i'm 3rd year student pursuing B.tech, CSE(core ) from VIT Chennai ,i'm interested in problem solving ,Web development and UI/UX.I'm quite good in solving coding questions and improve my skills day to day which helps me in real time world and i have a good communicational skills which helps me in gathering of information",
+    your_img_url: "https://www.itprotoday.com/sites/itprotoday.com/files/styles/article_featured_retina/public/programming%20evolution.jpg?itok=WTj9-yNz",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "My name is Abhishek Kumar and i'm 3rd year student pursuing B.tech, CSE(core ) from VIT Chennai ,i'm interested in problem solving ,Web development and UI/UX.I'm quite good in solving coding questions and improve my skills day to day which helps me in real time world and i have a good communicational skills which helps me in gathering of information",
+};
+const worktimeline = [{
+        jobtitle: "Data Science intern ",
+        where: "Capabl",
+        date: "2023",
+    },
+    {
+        jobtitle: "Web Dev. Team Member",
+        where: "Microsoft Innovation Club",
+        date: "2023",
+    },
+  
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C++",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "HTML/CSS",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "MongoDB",
+        value: 60,
+    },
+    
+    {
+        name:"NodeJS",
+        value:60,
+    },
+
+];
+
+
+
+const dataportfolio = [{
+        img: "https://cdn-icons-png.flaticon.com/512/7477/7477790.png",
+        description: "Simon game of colors",
+        link: "https://abhishekkumargithub.github.io/simongame/",
+    },
+    {
+        img: "https://developer.redis.com/assets/images/chatapp_homepage-e18893a7fe583a1dbda4c15347ee9d52.png",
+        description: "Real-time chat application",
+        link: "#",
+    },
+    
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abhishek.kumar2021d@vitstudent.ac.in",
+    YOUR_FONE: "9782477058",
+    description: "Message me",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "#",
+    facebook: "#",
+    linkedin: "#",
+    twitter: "#",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
